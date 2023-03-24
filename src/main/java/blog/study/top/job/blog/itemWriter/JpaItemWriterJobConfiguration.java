bf5e74e72459c6f0cb173eaaea9538dd1eb12c0c
inwookjung <inwook.jung@kr.doubledown.com>
2023-03-25 02:27:47 +0900

    batch itemWriter 공부

---
+++ src/main/java/blog/study/top/job/blog/itemWriter/JpaItemWriterJobConfiguration.java
@@ -0,0 +1,68 @@
+package blog.study.top.job.blog.itemWriter;
+
+import blog.study.top.entity.Pay;
+import jakarta.persistence.EntityManagerFactory;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.database.JpaItemWriter;
+import org.springframework.batch.item.database.JpaPagingItemReader;
+import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Slf4j
+@RequiredArgsConstructor
+@Configuration
+public class JpaItemWriterJobConfiguration {
+
+	private final JobRepository jobRepository;
+	private final PlatformTransactionManager transactionManager;
+	private final EntityManagerFactory entityManagerFactory;
+	private int CHUNK_SIZE = 10;
+
+	@Bean
+	public Job jpaItemWriterJob() {
+		return new JobBuilder("jpaItemWriterJob", jobRepository)
+				.start(jpaItemWriterStep())
+				.build();
+	}
+
+	@Bean
+	public Step jpaItemWriterStep() {
+		return new StepBuilder("jpaItemWriterStep", jobRepository)
+				.<Pay, Pay>chunk(CHUNK_SIZE, transactionManager)
+				.reader(jpaItemWriterReader())
+				.processor(jpaItemProcessor())
+				.writer(jpaItemWriter())
+				.build();
+	}
+
+	@Bean
+	public JpaPagingItemReader<Pay> jpaItemWriterReader() {
+		return new JpaPagingItemReaderBuilder<Pay>()
+				.name("jpaItemWriterReader")
+				.entityManagerFactory(entityManagerFactory)
+				.pageSize(CHUNK_SIZE)
+				.queryString("SELECT p FROM Pay p")
+				.build();
+	}
+
+	@Bean
+	public ItemProcessor<Pay, Pay> jpaItemProcessor() {
+		return pay -> pay;
+	}
+
+	@Bean
+	public JpaItemWriter<Pay> jpaItemWriter() {
+		JpaItemWriter<Pay> jpaItemWriter = new JpaItemWriter<>();
+		jpaItemWriter.setEntityManagerFactory(entityManagerFactory);
+		return jpaItemWriter;
+	}
+}
