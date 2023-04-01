0ddba88c56d133f11bed15768f25d520275a8bc6
inwookjung <inwook.jung@kr.doubledown.com>
2023-04-02 02:03:16 +0900

    spring batch paging reader 사용시 같은 조건의 데이터를 읽고 수정할때 문제

--- src/main/java/blog/study/top/repository/pay/PayRepository.java
+++ src/main/java/blog/study/top/repository/pay/PayRepository.java
@@ -1,7 +1,11 @@
 package blog.study.top.repository.pay;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PayRepository extends JpaRepository<PayEntity, Long> {
 
+	@Query("SELECT p FROM pay p where p.successStatus = true")
+	List<PayEntity> findAllSuccess();
 }
